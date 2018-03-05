e92ae1752e7d0f6c56ede379d604478c28cf824f
Tonio <antoine.doussan@campus-numerique-in-the-alps.com>
2018-03-05 11:38:15 +0100

    Début projet

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img src="http://www.clker.com/cliparts/A/q/F/O/c/o/coffee-cup-md.png">
+    <h1>{{ msg }}</h1>
+    <h2>Choisissez un module:</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+    </ul>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'COFFEE AROUND THE WORLD...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
