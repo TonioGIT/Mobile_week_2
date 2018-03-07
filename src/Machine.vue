dbb799207a883610e4d52cce3dd9d9101022f406
Tonio <antoine.doussan@campus-numerique-in-the-alps.com>
2018-03-07 09:49:50 +0100

    Etape 6 ok, boucle sur liste de machines

--- src/Machine.vue
+++ src/Machine.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <br>
-        <h1>{{ machineName }} : {{name}}</h1>
+        <h1>{{ machineName }} {{name}}</h1>
         <h2 id="red" v-if="status" >{{machineStat}} KO...</h2>
         <h2 id="green" v-else>{{machineStat}} OK !</h2>
-        <h2>{{ machineDate }} : {{checkedAt}}</h2>        
+        <h2>{{ machineDate }} {{checkedAt}}</h2>        
     </div>
 </template>
 
@@ -15,7 +15,7 @@
             return {
                 machineName: 'Machine name: ',
                 machineStat: 'Status: ',
-                machineDate: 'Last time checked: ',            
+                machineDate: 'Last time checked: ',    
             //   machine:{  
             //     name: 'Berlin Coffee Machine',
             //     status: false,
--- src/MachinesList.vue
+++ src/MachinesList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Liste des machines</h1> 
-        <machine-compo v-bind:name="machine.name" 
+        <machine-compo id="listcadres" v-for="machine in machines" :key="machine.id" v-bind:name="machine.name" 
         v-bind:status="machine.status" 
         v-bind:checkedAt="machine.checkedAt">
         </machine-compo>
@@ -13,16 +13,42 @@ var event = new Date();
     export default {
         data() {
             return {
-            machine: {  
-                name: 'Berlin Coffee Machine',
-                status: false,
-                checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
-              }
+            // machine: {  
+            //     name: 'Berlin Coffee Machine',
+            //     status: false,
+            //     checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
+            //   }
+                machines: [{
+                    id: 1,
+                    name: 'BLUE OCEAN',
+                    status: true,
+                    checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
+                }, {
+                    id: 2,
+                    name: 'BERRY CRUSH',
+                    status: false,
+                    checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
+                }, {
+                    id: 3,
+                    name: 'WHITE SOVIET',
+                    status: true,
+                    checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
+                }, {
+                    id: 4,
+                    name: 'PINK LADY',
+                    status: false,
+                    checkedAt: event.toLocaleString('en-FR', { timeZone: 'UTC' }),
+                }]
             }
         }
 }
 </script>
 
 <style scoped>
+
+#listcadres {
+    border: 3px solid #0a3c59;
+    margin: 10.5px 21px;
+}
 
 </style>
